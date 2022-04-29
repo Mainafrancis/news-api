776977ef8a3819c57c3bec641f50894359725d70
Mainafrancis <francis.maina@student.moringaschool.com>
2022-04-29 15:33:41 +0300

    unittest

--- manage.py
+++ manage.py
@@ -3,3 +3,16 @@ from flask__script import Manager,Server
 
 #Creating app instance
 app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """Run the unit test"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    manager.run()   
