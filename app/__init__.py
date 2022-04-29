e6060135d189323b83cbd6746cc41f8f54c242fb
Mainafrancis <francis.maina@student.moringaschool.com>
2022-04-29 16:04:13 +0300

    extensions

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,15 @@ from flask_bootsrap import Bootsrap
 
 #initializing bootsrap extension
 bootstrap = Bootsrap()
+
+def create_app(config_name):
+
+    #initializing application
+    app = Flask(__name__,instance_relative_config = True)
+
+    #creating configurations
+    app.config.from_object(config_options[config_name])
+
+    #initialize flask extensions
+    bootstrap.init_app(app)
+
