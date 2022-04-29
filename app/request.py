8c98044167495cded2e61df89d4dbb87c0ec9f68
Mainafrancis <francis.maina@student.moringaschool.com>
2022-04-29 16:34:05 +0300

    get results

--- app/request.py
+++ app/request.py
@@ -8,3 +8,12 @@ def configure_request(app):
     global api_key, base_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
+
+def get_news(category):
+    get_news_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
